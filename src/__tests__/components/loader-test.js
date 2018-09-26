e2c70e35df0fe47d0e4ee65ac03a05daa4a170a5
rap2h <raphaelht@gmail.com>
2018-09-26 15:02:24 +0200

    only one test

--- src/__tests__/components/loader-test.js
+++ src/__tests__/components/loader-test.js
@@ -7,16 +7,4 @@ describe('Loader suite', () => {
   it('should render without throwing an error', () => {
     expect(shallow(<Loader />).contains(<div id="loader"/>)).toBe(true);
   });
-
-  it('should be selectable by class "loader-container"', () => {
-    expect(shallow(<Loader />).is('.loader-container')).toBe(true);
-  });
-
-  it('should mount in a full DOM', () => {
-    expect(mount(<Loader />).find('.loader-container').length).toBe(1);
-  });
-
-  it('should render to static HTML', () => {
-    expect(render(<Loader />).text()).toEqual('');
-  });
 });
